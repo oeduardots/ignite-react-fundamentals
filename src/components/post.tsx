f753844ce9c706061380824c78733d54c9c3e1d6
Eduardo Tavares <edutav.dev@gmail.com>
2024-05-26 00:40:39 -0300

    estado usestate

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,11 +1,14 @@
 import { format, formatDistanceToNow } from 'date-fns'
 import { ptBR } from 'date-fns/locale/pt-BR'
+import { useState } from 'react'
 
 import { Avatar } from './avatar'
 import { Comment } from './comment'
 import styles from './post.module.css'
 
 export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([1, 2])
+
   const publishedDateFormatted = format(
     publishedAt,
     "d 'de' LLLL 'às' HH:mm'h'",
@@ -18,6 +21,13 @@ export function Post({ author, publishedAt, content }) {
     locale: ptBR,
     addSuffix: true,
   })
+
+  function handleCrateNewComment() {
+    event.preventDefault()
+
+    // imutabilidade
+    setComments([...comments, comments.length + 1])
+  }
 
   return (
     <article className={styles.post}>
@@ -52,7 +62,7 @@ export function Post({ author, publishedAt, content }) {
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCrateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
         <textarea placeholder="Deixe um comentário" />
@@ -63,9 +73,9 @@ export function Post({ author, publishedAt, content }) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map((comment) => {
+          return <Comment />
+        })}
       </div>
     </article>
   )

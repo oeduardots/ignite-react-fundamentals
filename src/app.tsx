7ebbfdf1441d1e5db76909ef6c8654cf6f418cbb
Eduardo Tavares <edutav.dev@gmail.com>
2024-05-25 21:58:37 -0300

    componente: sidebar

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,17 @@
 import './global.css'
 
+import styles from './app.module.css'
 import { Header } from './components/header'
+import { Sidebar } from './components/sidebar'
 
 export function App() {
   return (
     <div>
       <Header />
+
+      <main className={styles.wrapper}>
+        <Sidebar />
+      </main>
     </div>
   )
 }
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,22 @@
+import styles from './sidebar.module.css'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1562907550-096d3bf9b25c?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt=""
+      />
+
+      <div className={styles.profile}>
+        <strong>Eduardo Tavares</strong>
+        <span>Desenvolvedor Front-end</span>
+      </div>
+
+      <footer>
+        <a href="#">Editar seu perfil</a>
+      </footer>
+    </aside>
+  )
+}

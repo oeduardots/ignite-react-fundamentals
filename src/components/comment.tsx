524611c0339510f84fe3cb318a48d6e1a97f6299
Eduardo Tavares <edutav.dev@gmail.com>
2024-05-26 17:26:39 -0300

    comunicacao entre componentes

--- src/components/comment.tsx
+++ src/components/comment.tsx
@@ -3,7 +3,11 @@ import { ThumbsUp, Trash } from '@phosphor-icons/react'
 import { Avatar } from './avatar'
 import styles from './comment.module.css'
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/diego3g.png" />
@@ -18,7 +22,7 @@ export function Comment({ content }) {
               </time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>

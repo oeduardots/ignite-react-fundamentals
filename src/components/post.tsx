694e22fab6e355d545acca1b4c450d0f9ef0d3e9
Eduardo Tavares <edutav.dev@gmail.com>
2024-05-27 00:49:12 -0300

    validando formulario

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -39,7 +39,12 @@ export function Post({ author, publishedAt, content }) {
   }
 
   function handleNewCommentChange() {
+    event?.target.setCustomValidity('')
     setNewCommentText(event?.target.value)
+  }
+
+  function handleNewCommentInvalid() {
+    event?.target.setCustomValidity('Este campo é obrigatório')
   }
 
   function deleteComment(commentToDelete) {
@@ -50,6 +55,8 @@ export function Post({ author, publishedAt, content }) {
     })
     setComments(commentsWithoutDeletedOne)
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0
 
   return (
     <article className={styles.post}>
@@ -93,10 +100,14 @@ export function Post({ author, publishedAt, content }) {
           onChange={handleNewCommentChange}
           // programação declarativa
           value={newCommentText}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
         </footer>
       </form>
 

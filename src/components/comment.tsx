faad5e7080d8faf702d3dcb98feae4944cab3e86
Eduardo Tavares <edutav.dev@gmail.com>
2024-05-27 01:04:26 -0300

    realizando likes nos comentarios

--- src/components/comment.tsx
+++ src/components/comment.tsx
@@ -1,11 +1,18 @@
 import { ThumbsUp, Trash } from '@phosphor-icons/react'
+import { useState } from 'react'
 
 import { Avatar } from './avatar'
 import styles from './comment.module.css'
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0)
+
   function handleDeleteComment() {
     onDeleteComment(content)
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1)
   }
 
   return (
@@ -31,9 +38,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
